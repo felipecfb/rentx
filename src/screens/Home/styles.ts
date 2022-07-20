f4065eba82a4b16ebd58d1a36b287b9dd5269ecd
Felipe Bastos | DT3 <felipe.bastos@dt3.com.br>
2022-07-20 14:49:41 -0300

    feat: criando screen Home

---
+++ src/screens/Home/styles.ts
@@ -0,0 +1,11 @@
+import React from "react";
+
+import styled from "styled-components/native";
+
+export const Container = styled.View`
+  flex: 1;
+  justify-content: center;
+  align-items: center;
+`;
+
+export const Title = styled.Text``;
