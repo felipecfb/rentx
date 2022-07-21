11551239b61de44f346a791bc6ab5c4829d8d290
Felipe Bastos | DT3 <felipe.bastos@dt3.com.br>
2022-07-21 14:31:31 -0300

    feat: criando interface de agendamento

--- App.tsx
+++ App.tsx
@@ -15,6 +15,7 @@ import {
 import { Home } from "./src/screens/Home";
 import theme from "./src/styles/theme";
 import { CarDetails } from "./src/screens/CarDetails";
+import { Scheduling } from "./src/screens/Scheduling";
 
 export default function App() {
   const [appIsReady, setAppIsReady] = useState(false);
@@ -58,7 +59,7 @@ export default function App() {
           flex: 1,
         }}
       >
-        <CarDetails />
+        <Scheduling />
       </View>
     </ThemeProvider>
   );
