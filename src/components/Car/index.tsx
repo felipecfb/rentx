514a4201352e425ae015c236c3925a34d1ddd6cc
Felipe Bastos | DT3 <felipe.bastos@dt3.com.br>
2022-07-21 10:47:47 -0300

    feat: criando componente card car

---
+++ src/components/Car/index.tsx
@@ -0,0 +1,44 @@
+import React from "react";
+
+import * as S from "./styles";
+
+import GasolineSvg from "../../assets/gasoline.svg";
+
+interface CarData {
+  brand: string;
+  name: string;
+  rent: {
+    period: string;
+    price: number;
+  },
+  thumbnail: string;
+}
+
+interface Props {
+  data: CarData;
+  
+}
+
+export function Car({ data }: Props) {
+  return (
+    <S.Container>
+      <S.Details>
+        <S.Brand>{data.brand}</S.Brand>
+        <S.Name>{data.name}</S.Name>
+
+        <S.About>
+          <S.Rent>
+            <S.Period>{data.rent.period}</S.Period>
+          <S.Price>{`R$ ${data.rent.price}`}</S.Price>
+          </S.Rent>
+
+          <S.Type>
+            <GasolineSvg />
+          </S.Type>
+        </S.About>
+      </S.Details>
+
+      <S.CarImage source={{ uri: data.thumbnail }} resizeMode="contain" />
+    </S.Container>
+  );
+}
--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -3,10 +3,31 @@ import { StatusBar } from "react-native";
 import { RFValue } from "react-native-responsive-fontsize";
 
 import Logo from "../../assets/logo.svg";
+import { Car } from "../../components/Car";
 
 import * as S from "./styles";
 
 export function Home() {
+  const CarDataOne = {
+    brand: "Audi",
+    name: "RS 5 Coupé",
+    rent: {
+      period: "Ao dia",
+      price: 120,
+    },
+    thumbnail: "https://pensecarros.com.br/cms/uploads/audi-rs5-2-9-v6-tfsi-gasolina-sportback-quattro-s-tronic-605a9d69353b2.png",
+  }
+
+  const CarDataTwo = {
+    brand: "Prosche",
+    name: "Panamera",
+    rent: {
+      period: "Ao dia",
+      price: 340,
+    },
+    thumbnail: "https://www.webmotors.com.br/imagens/prod/348928/PORSCHE_PANAMERA_2.9_V6_EHYBRID_4_PLATINUM_EDITION_PDK_34892815305718989.webp?s=fill&w=130&h=97&q=70&t=true)",
+  }
+
   return (
     <S.Container>
       <StatusBar
@@ -20,6 +41,9 @@ export function Home() {
         <S.TotalCars>Total de 12 carros</S.TotalCars>
         </S.HeaderContent>
       </S.Header>
+
+      <Car data={CarDataOne} />
+      <Car data={CarDataTwo} />
     </S.Container>
   );
 }
