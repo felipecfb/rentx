f0bc476dc6b3074be611f29c9620195839e747b8
Felipe Bastos | DT3 <felipe.bastos@dt3.com.br>
2022-07-25 10:29:31 -0300

    feat: finalizando interface de agendamento

--- src/screens/SchedulingComplete/index.tsx
+++ src/screens/SchedulingComplete/index.tsx
@@ -5,12 +5,26 @@ import * as S from "./styles";
 
 import LogoSvg from "../../assets/logo_background_gray.svg";
 import DoneSvg from "../../assets/done.svg";
+import { ConfirmButton } from "../../components/ConfirmButton";
 
 export function SchedulingComplete() {
   const { width } = useWindowDimensions();
   return (
     <S.Container>
       <LogoSvg width={width} />
+
+      <S.Content>
+        <DoneSvg width={80} height={80} />
+        <S.Title>Carro alugado!</S.Title>
+        <S.Message>
+          Agora você só precisa ir {"\n"} até a concessionária da RENTX {"\n"}
+          pegar o seu automóvel
+        </S.Message>
+      </S.Content>
+
+      <S.Footer>
+        <ConfirmButton title="Ok" />
+      </S.Footer>
     </S.Container>
   );
 }
