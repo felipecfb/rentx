daa0b33fd983734d725518027264a60f1c28f027
Felipe Bastos <felipe.bastos.dev@gmail.com>
2022-11-01 11:46:20 -0300

    ✨ feat: listando agendamento de usuários

--- src/routes/stack.routes.tsx
+++ src/routes/stack.routes.tsx
@@ -6,6 +6,7 @@ import { CarDetails } from "../screens/CarDetails";
 import { Scheduling } from "../screens/Scheduling";
 import { SchedulingDetails } from "../screens/SchedulingDetails";
 import { SchedulingComplete } from "../screens/SchedulingComplete";
+import { MyCars } from "../screens/MyCars";
 
 const { Navigator, Screen } = createStackNavigator();
 
@@ -19,6 +20,7 @@ export function StackRoutes() {
       <Screen name="Scheduling" component={Scheduling} />
       <Screen name="Scheduling Details" component={SchedulingDetails} />
       <Screen name="Scheduling Complete" component={SchedulingComplete} />
+      <Screen name="MyCars" component={MyCars} />
     </Navigator>
   );
 }
