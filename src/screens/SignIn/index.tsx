901e642746c6a5e033ea26b686cc7170a92260ad
Felipe Bastos <felipe.bastos.dev@gmail.com>
2023-05-12 17:29:53 -0300

    feat: validando o form

--- src/screens/SignIn/index.tsx
+++ src/screens/SignIn/index.tsx
@@ -1,15 +1,40 @@
 import React, { useState } from 'react';
+import { KeyboardAvoidingView, StatusBar, TouchableWithoutFeedback, Keyboard, Alert } from 'react-native';
+import * as Yup from 'yup'
 
-import * as S from './styles';
-import { KeyboardAvoidingView, StatusBar, TouchableWithoutFeedback, Keyboard } from 'react-native';
 import { Button } from '../../components/Button';
-import theme from '../../styles/theme';
 import { Input } from '../../components/Input';
 import { PasswordInput } from '../../components/PasswordInput';
+
+import theme from '../../styles/theme';
+import * as S from './styles';
 
 export function SignIn() {
   const [email, setEmail] = useState('')
   const [password, setPassword] = useState('')
+
+  async function handleSignIn() {
+    try {
+      const schema = Yup.object().shape({
+        email: Yup.string()
+          .required('E-mail obrigatório')
+          .email('Digite um e-mail válido'),
+        password: Yup.string()
+          .required('A senha é obrigatória')
+      })
+
+      await schema.validate({ email, password })
+      Alert.alert('Tudo certo!')
+
+      // Fazer login.
+    } catch (err) {
+      if (err instanceof Yup.ValidationError) {
+        return Alert.alert('Opa', err.message)
+      }
+
+      return Alert.alert('Erro na autenticação', 'Ocorreu um erro ao fazer login, verifique as credenciais')
+    }
+  }
 
   return (
     <KeyboardAvoidingView
@@ -54,8 +79,8 @@ export function SignIn() {
           <S.Footer>
             <Button
               title="Login"
-              onPress={() => { }}
-              enabled={false}
+              onPress={handleSignIn}
+              enabled={true}
               loading={false}
             />
             <Button
