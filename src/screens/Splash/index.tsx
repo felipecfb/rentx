7d5f6e9d064b3305a99be01f091f0775b7b0c0cd
Felipe Bastos <felipe.bastos.dev@gmail.com>
2023-05-12 08:41:16 -0300

    feat: criando primeira animação

--- src/screens/Splash/index.tsx
+++ src/screens/Splash/index.tsx
@@ -1,11 +1,37 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { Button, StyleSheet } from 'react-native';
+
+import Animated, { useAnimatedStyle, useSharedValue } from 'react-native-reanimated';
 
 import * as S from './styles';
 
 export function Splash() {
+  const animation = useSharedValue(0);
+
+  const animatedStyles = useAnimatedStyle(() => {
+    return {
+      transform: [{ translateX: animation.value }]
+    }
+  });
+
+  function handleAnimationPosition () {
+    animation.value = Math.random() * 500;
+  }
+
   return (
     <S.Container>
-      
+      <Animated.View style={[styles.box, animatedStyles]} />
+
+
+      <Button title="Mover" onPress={handleAnimationPosition} />
     </S.Container>
   );
-}
+};
+
+const styles = StyleSheet.create({
+  box: {
+    width: 100,
+    height: 100,
+    backgroundColor: 'red'
+  }
+});
